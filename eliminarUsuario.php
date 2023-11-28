fc3b97d5ff4b15c1c1cd69c60c8d6a461fcd95be
Angela Atenea Larios Gutierrez. Tony <ateneagutierrez17@aragon.unam.mx>
2023-11-28 12:55:26 -0600

    4to avance

---
+++ eliminarUsuario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="HTML, UA, Prueba">
+        <meta name="description" content="Página de prueba">
+        <meta name="author" content="Atenea Gutiérrez / Tony Gutiérrez / MissionApolo18">
+        <link rel="icon" href="Recursos/UA_logo.png" type="image/png" sizes="18x18">
+        <link rel="stylesheet" href="Estilos.css">
+        <title>UA Borrar Usuario</title>
+    </head>
+
+    <body>
+        <h1 style=" text-align: center;" >Elimina Usuario</h1>
+        <form style=" text-align: center;" method="POST" action="delet.php">
+            <input  type="text" name="no_cuenta" placeholder="Numero de Cuenta" />
+            <br />
+            <button type="submit">Eliminar usuario</button>
+
+        </form>
+
+        <h1><a href="Login.html">Inicio de listas</a></h1>
+
+    </body>
+
+</html>
