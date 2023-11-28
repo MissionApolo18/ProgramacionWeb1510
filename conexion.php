09865e69441d0274761d9d79f722b774a5d1b686
Angela Atenea Larios Gutierrez. Tony <ateneagutierrez17@aragon.unam.mx>
2023-11-28 11:08:42 -0600

    Tercer avance con la bdd

--- conexion.php
+++ conexion.php
@@ -1,10 +1,15 @@
 <?php
+    $host_db = "localhost:3328";
+    $user_db = "root";
+    $pass_db = "";
+    $db_name = "login_registro_db";
+    $tbl_name = "usuarios";
+
     $conexion = mysqli_connect("localhost","root","","login_registro_db");
 
-    /*
     if($conexion){
         echo 'Conectado exitosamente';
     } else {
         echo 'No se pudo conectar a la base de datos';
-    }*/
+    }
 ?>
--- registro_usuario.php
+++ registro_usuario.php
@@ -1,13 +1,38 @@
 <?php
     include 'conexion.php';
+    mysqli_set_charset($conexion,'utf8');
 
     $nombre_completo = $_POST['nombre_completo'];
     $correo = $_POST['correo'];
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    $unir ="INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena) 
-            VALUES('$nombre_completo','$correo','$usuario','$contrasena')";
+    $usuarioDisponible = "SELECT * FROM usuarios where usuario = '$_POST[usuario]'";
+    $result = $conexion -> query($buscarUsuario);
+    $count = mysqli_num_rows($result);
 
-    $ejecutar = mysqli_query($conexion, $unir);
+    if($count ==1 ){
+        echo'El nombre se usuario ya a sido ocupado';
+        header('Location: ./form_registro.php');
+        
+    }else{
+        mysqli_query($conexion, "INSERT INTO usuarios (
+        nombre,
+        direccion,
+        telefono,
+        correo,
+        nombre_usuario,
+        password)
+            VALUES(
+        '$_POST[usuario]',
+        '$_POST[correo]',
+        '$_POST[nombre_completo]',
+        '$_POST[contrasena]'        
+        )");
+
+    echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
+    echo "<h4>" . "Bienvenido: " . $_POST['usuario'] . "</h4>" . "\n\n";
+    echo "<h5>" . "<a href='./form_registro.php'>Registro</a>" . "</h5>";
+    }
+
 ?>
